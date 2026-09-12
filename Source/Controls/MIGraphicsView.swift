493878ad1cabbd718b9bbd368603e1271c559dcc
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-09-13 08:28:04 +0900

    MIGraphicsView: Initial commit

---
+++ Source/Controls/MIGraphicsView.swift
@@ -0,0 +1,34 @@
+/*
+ * @file MIGraphicsView.swift
+ * @description Define MIGraphicsView class
+ * @par Copyright
+ *   Copyright (C) 2024 Steel Wheels Project
+ */
+
+import  MultiUIKit
+#if os(OSX)
+import  AppKit
+#else   // os(OSX)
+import  UIKit
+#endif  // os(OSX)
+
+open class MIGraphicsView: MIInterfaceView
+{
+        public typealias ButtonPressedCallback = MIButtonCore.ButtonPressedCallback
+
+        public override func setup(frame frm: CGRect) {
+                super.setup(nibName: "MIMetalCoreView", frameSize: frm.size)
+        }
+
+        private func coreGraphicsView() -> MIMetalCoreView {
+                if let core: MIMetalCoreView = super.coreView() {
+                        return core
+                } else {
+                        fatalError("Failed to get core view")
+                }
+        } 
+}
+
+
+
+
