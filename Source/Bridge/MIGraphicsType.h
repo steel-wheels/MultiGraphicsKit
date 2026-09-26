ef70f35a31b028404776a907f2b3174eb7c80d27
Steel Wheels Project <wheel@tcn.zaq.ne.jp>
2026-09-26 17:20:31 +0900

    MIGraphicsType: Initial commit

---
+++ Source/Bridge/MIGraphicsType.h
@@ -0,0 +1,17 @@
+/*
+ * @file MIGraphicsType.h
+ * @description Define graphis type to share with metal
+ * @par Copyright
+ *   Copyright (C) 2026 Steel Wheels Project
+ */
+
+#ifndef MIGraphicsType_h
+#define MIGraphicsType_h
+
+# include <simd/simd.h>
+
+typedef struct {
+        simd_float2     position ;
+} GPoint2f ;
+
+#endif /* MIGraphicsType_h */
---
+++ Source/Bridge/MultiGraphicsKit.h
@@ -0,0 +1,13 @@
+/*
+ * @file MultiGraphicsKit.h
+ * @description Umbrella type
+ * @par Copyright
+ *   Copyright (C) 2026 Steel Wheels Project
+ */
+
+#ifndef MULTIGRAPHICSKIT_H
+#define MULTIGRAPHICSKIT_H
+
+#include <MultiGraphicsKit/MIGraphicsType.h>
+
+#endif /* MULTIGRAPHICSKIT_H */
